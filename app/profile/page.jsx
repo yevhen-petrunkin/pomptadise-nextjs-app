9b55ffe77a52fd0d5c7da44e446bf2cb14c4e3e2
yevhen-petrunkin <novamovaxxi@gmail.com>
2023-07-12 22:32:45 +0300

    Add edit and delete prompt functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -8,6 +8,7 @@ import Profile from "@components/Profile";
 
 const MyProfile = () => {
   const { data: session } = useSession();
+  const router = useRouter();
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -22,9 +23,29 @@ const MyProfile = () => {
     }
   }, []);
 
-  const handleEdit = () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error, error.message);
+      }
+    }
+  };
 
   return (
     <Profile
