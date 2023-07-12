b04447b20c229426a99bf821b7916bfc9e8e1c36
yevhen-petrunkin <novamovaxxi@gmail.com>
2023-07-12 13:45:34 +0300

    Fix secret leak, add create-prompt page

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+
+  return <Form />;
+};
+
+export default CreatePrompt;
